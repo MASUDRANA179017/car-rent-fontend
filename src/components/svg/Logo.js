8b2981d8c93133040bca5d4fff8b0b52e641d7c2
Masud Rana <68780186+MASUDRANA179017@users.noreply.github.com>
2024-01-19 01:24:46 +0600

    Initial commit

---
+++ src/components/svg/Logo.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <>
+      <a href="/" className="text-red-700 uppercase text-lg font-bold">Car Rent</a>
+    </>
+  );
+};
+
+export default Logo;
