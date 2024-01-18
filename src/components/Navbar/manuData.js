f9f6266d1b3d21c00fc973137da0a14c3e0d71d5
Masud Rana <68780186+MASUDRANA179017@users.noreply.github.com>
2024-01-19 01:24:46 +0600

    Initial commit

---
+++ src/components/Navbar/manuData.js
@@ -0,0 +1,19 @@
+const manuData = [
+    {
+        title: "Home",
+        path: "/",
+    },
+    {
+        title: "About",
+        path: "/pages/about",
+    },
+    {
+        title: "Contact",
+        path: "/",
+    },
+    {
+        title: "My History",
+        path: "/pages/signin",
+    },
+]
+export default manuData
